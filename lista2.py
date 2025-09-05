5a5cb622256027f9dcd57e5fc7d8fa1605676939
greici000 <carvalho.r@estudante.ifmt.edu.br>
2025-09-05 11:58:33 -0400

    Update lista2.py
    continuar com as resoluções dos exercicios apartir da questão de numero 13

--- lista2.py
+++ lista2.py
@@ -167,13 +167,14 @@ def exercicio12()
         print( " Menor de idade")
     if idade > 65 : 
         print( " Maior de 65 anos")
+        
 
 #13. Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota
 #da prova 2 de um aluno. O programa deve imprimir o nome, a nota da prova 1,
 #a nota da prova 2, a média das notas e uma das mensagens: "Aprovado",
 #Reprovado"ou "em Prova Final"(a média é 7 para aprovação, menor que 3 para
 #reprovação e as demais em prova final
-
+def exercicio13()
 
 #14. Faça um programa que permita entrar com o salário de uma pessoa e imprima o
 #desconto do INSS segundo a tabela seguir:
