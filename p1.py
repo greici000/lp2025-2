4d887b7b81095fc9ddda5d757e6f051654848698
greici000 <carvalho.r@estudante.ifmt.edu.br>
2025-10-17 15:59:44 +0000

    p1

--- p1.py
+++ p1.py
@@ -1,7 +1,7 @@
 # Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota da prova 2 de um aluno.
 # O programa deve imprimir o nome, a nota da prova 1, a nota da prova 2, a média das notas e uma das mendagens:
 # "Aprovado," "Reprovado" ou "em Prova final".
-
+def q1():
     nome = input('Digite o nome do aluno') 
     n1 = float(input('digite a primeira nota  prova 1: ')) # o float( tipo real) input(espera que o usuario digite um texto)
     n2 = float(input('Digite a segunda nota prova 2: '))
@@ -19,7 +19,7 @@
 # Crie um programa que permita entrar com o nome, a nota da prova 1 e da prova 2 de 15 alunos.
 # Ao final, imprima uma listagem, contendo: nome, nota prova 1, nota prova 2, e média de cada aluno.
 # Ao final, imprimir a média geral da turma.
-
+def q2():
     Num_ALUNOS = 15
     
     soma_das_medias = 0.0
@@ -35,8 +35,8 @@
 # Crie um programa que leia asnotas de 'N' alunos de uma turma (onde 'N' é um numero fornecido pelo usuario.)
 # Ao final, o programa deve calcular: a media da turma, o percentual de reprovados(considerando
 # reprovação se a nota for inferior a 7.0).
-
-print("--- Análise de Notas por Turma ---")
+def q3():
+    print("--- Análise de Notas por Turma ---")
     num_alunos = int(input("Número de alunos da turma (0 para sair): "))
     if num_alunos <= 0: return
 
@@ -59,8 +59,8 @@ print("--- Análise de Notas por Turma ---")
 # valor da prestação vencida, a taxa periódica de juros e o peíodo de atraso. 
 # Ao final, o programa deve imprimir o valor da prestação atrasada, o periodo de atraso,
 # os juros que serão cobrados pelo período de atraso, e o valor da prestação acrescido dos juros 
-
- valor_prestacao = float(input("Valor da prestação vencida: R$ "))
+def q4():
+    valor_prestacao = float(input("Valor da prestação vencida: R$ "))
     taxa_juros = float(input("Taxa de juros (ex: 0.02 para 2%): "))
     periodo_atraso = float(input("Período de atraso: "))
     
@@ -73,8 +73,8 @@ print("--- Análise de Notas por Turma ---")
 # A campanhia de Emprétimos xxx só concede empréstimo se o valor da prestação não ultrapassar 
 # 30% do salário bruto do proponente. Faça um programa que leia o sálario bruto e o valor da 
 # prestação e, ao final, imprima se o empréstimo pode ou não ser concedido.
-
- salario = float(input('Salário bruto: R$ '))
+def q5():
+    salario = float(input('Salário bruto: R$ '))
     prestacao = float(input('Valor da Prestação: R$ '))
     prestacao_maxima = salario * 0.3
     if prestacao > prestacao_maxima:
@@ -85,8 +85,8 @@ print("--- Análise de Notas por Turma ---")
 # Faça um programa que calcule o reajuste do salário de um funcionário.
 # Para isso, o programa deverá ler o salário atual do funcionario e ler o 
 # percentual de reajuste. Ao final imprimir o valor do novo salário.
-
- salario = float(input("Salário atual: R$ "))
+def q6():
+    salario = float(input("Salário atual: R$ "))
     percentual_reajuste = float(input("Percentual do reajuste: "))
     reajuste = salario * (percentual_reajuste / 100)
     novo_salario = salario + reajuste
@@ -96,7 +96,8 @@ print("--- Análise de Notas por Turma ---")
 # Cada pedido é composto pelos seguintes campos: número de pedido, data do pedido(dia,mês,ano)
 # preço unitário, quantidade. O programa deverá processar novos pedidos até que o usuário digite 0 (zero)
 # como número do pedido.
-total_compra_geral = 0.0
+def q7():
+    total_compra_geral = 0.0
     print("Digite 0 como número do pedido para encerrar.")
     while True:
         num_pedido = int(input("\nNúmero do pedido (0 para sair): "))
@@ -112,8 +113,8 @@ total_compra_geral = 0.0
 #Faça um programa que leia dois números reais e calcule as quatro operações básicas entre estes
 # dois números: adição, subtração, multiplicação e divisão. Ao final, o programa deve imprimir
 # os resultados dos calcúlos.
-
-numero1 = float(input("Digite o primeiro número: "))
+def q8():
+    numero1 = float(input("Digite o primeiro número: "))
     numero2 = float(input("Digite o segundo número: "))
     adicao = numero1 + numero2
     multiplicao = numero1 * numero2
@@ -122,8 +123,8 @@ numero1 = float(input("Digite o primeiro número: "))
 
 # Faça um programa que efetue a apresentação do valor da conversão em real(R$) de um valor lido 
 # em dólar(U$$). O programa deve solicitar a cotação do dalar atual para realizar o cálculo.
-
-valor_dolar = float(input("Valor do dólar (US$): "))
+def q9():
+    valor_dolar = float(input("Valor do dólar (US$): "))
     cotacao_dolar = float(input("Cotação do dólar para real (R$): "))
     valor_real = valor_dolar * cotacao_dolar
     print(f"O valor de US${valor_dolar:.2f} é R${valor_real:.2f}")
@@ -133,8 +134,8 @@ valor_dolar = float(input("Valor do dólar (US$): "))
 # consumidos durante o mês, tipo (código) do consumidor (1- residencial, 2-comercial, 3-industrial)
 # Os dados devem ser lidos até que seja encontrado o consumiidor com número 0(zero).
 # O programa deve calcular e imprimir o custo total de cada consumidor.
-
-PRECOS = {1: 0.30, 2: 0.50, 3: 0.70}
+def q10():
+    PRECOS = {1: 0.30, 2: 0.50, 3: 0.70}
     print("Insira 0 no 'Número do consumidor' para finalizar.")
     while True:
         num_consumidor = int(input("\nNúmero do consumidor (0 para sair): "))
@@ -152,8 +153,8 @@ PRECOS = {1: 0.30, 2: 0.50, 3: 0.70}
 
 # Construa um programa que leia vários números e informe quantos números entre 100 e 200(inclusive)
 # foram digitados. Quando o valor 0(zero) for lido, o algoritimo deverá cessar sua exeção.
-
-contador = 0
+def q11():
+    contador = 0
     print("Digite 0 para encerrar.")
     while True:
         numero = int(input('Digite um número inteiro: '))
@@ -167,8 +168,8 @@ contador = 0
 
 # Faça um programa que leia um número inteiro entra 1 e 12 (referente a um mês do ano) e imprima
 # na tela o nome do mês correspondente. Se o número digitado não estiver neste intervalo, o programa de ve imprimir "Mês Invalido!"
-
- mes = int(input('Digite o número do mês: '))
+def q12():
+    mes = int(input('Digite o número do mês: '))
     if mes == 1:
         print('Janeiro')
     elif mes == 2:
@@ -176,3 +177,8 @@ contador = 0
     # ... continua até 12
     else:
         print('Mês Inválido!')
+
+
+
+questao = int(input('Questão a ser executada: '))
+eval(f'q{questao}()')
